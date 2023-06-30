5fd073c02b850c803683133e4e6e0d6d92f69eaf
zVima <115565123+zVima@users.noreply.github.com>
2023-06-30 15:51:51 +0200

    Update BoidManager.js

--- scripts/BoidManager.js
+++ scripts/BoidManager.js
@@ -1,76 +1,101 @@
-import { Vector2D } from './Vector2D.js';
-import { Boid } from './Boid.js';
-
-let section = document.getElementById('start-container').getBoundingClientRect();
-
-let width = section.width;
-let height = section.height;
-
 const canvas = document.getElementById('boids-canvas');
 const context = canvas.getContext('2d');
 
-canvas.height = height;
-canvas.width = width;
-
 const numOfBoids = 1000;
-const detectionRange = 10;
+const detectionRange = 75;
 
 const boids = [];
+
+let section;
+let width;
+let height;
+
+let boundary;
+let qTree;
+
+
+function updateCanvas() {
+  section = document.getElementById('start-container').getBoundingClientRect();
+  width = section.width;
+  height = section.height;
+  canvas.height = height;
+  canvas.width = width;
+}
 
 function createBoid() {
   const position = new Vector2D(Math.random() * section.width, Math.random() * section.height);
   const velocity = new Vector2D(Math.random() * 2 - 1, Math.random() * 2 - 1);
 
   const boid = new Boid(position, velocity, 3);
-  boid.draw(context);
   boids.push(boid);
+
+  qTree.insert(boid);
 }
 
-for (let i = 0; i < numOfBoids; i++) {
-  createBoid();
+function createQuadTree() {
+  boundary = new Rectangle(0, 0, width, height);
+  qTree = new QuadTree(boundary, 4);
 }
 
 function updateBoids() {
-  section = document.getElementById('start-container').getBoundingClientRect();
-  width = section.width;
-  height = section.height;
-  canvas.width = width;
-  canvas.height = height;
+  updateCanvas();
+  createQuadTree();
 
   context.clearRect(0, 0, width, height);
 
   // Update and draw each boid
   for (const boid of boids) {
-    // const nearbyBoids = [];
-
-    // for (const nearbyBoid of boids) {
-      // if (nearbyBoid !== boid) {
-        // const distance = boid.position.distance(nearbyBoid.position);
-        // if (distance < detectionRange) {
-          // nearbyBoids.push(nearbyBoid);
-        // }
-      // }
-      
-      // boid.velocity = calculateAlignment(boid, nearbyBoids);
-    // }
-
-    boid.update(width, height);
+    boid.update();
+    qTree.insert(boid);
     boid.draw(context);
+    let boidsInRange = [];
+    qTree.query(new Rectangle(boid.position.x - 50, boid.position.y - 50, 25, 25), boidsInRange);
+    
+    // drawBoidRange(boid);
+    // drawNearbyBoids(boidsInRange);
   }
 
+  qTree.show(context);
   requestAnimationFrame(updateBoids);
 }
 
-updateBoids();
+function drawBoidRange(boid) {
+  context.beginPath();
+  context.lineWidth = 2;
+  context.strokeStyle = "rgba(0, 255, 0, .2)";
+  context.rect(boid.position.x - detectionRange / 2, boid.position.y - detectionRange / 2, detectionRange, detectionRange);
+  context.stroke();
+}
+
+function drawNearbyBoids(nearbyBoids) {
+  for (let boid of nearbyBoids) {
+    const x = boid.position.x - (boid.size / 2);
+    const y = boid.position.y - (boid.size / 2);
+  
+    context.beginPath();
+    context.fillStyle = "green";
+    context.rect(x, y, boid.size, boid.size);
+    context.fill();
+  }
+}
 
 function calculateAlignment(boid, nearbyBoids) {
 
 }
 
-function calculateCohesion(boid, ) {
+function calculateCohesion(boid) {
 
 }
 
 function calculateSeperation() {
 
 }
+
+updateCanvas();
+createQuadTree();
+
+for (let i = 0; i < numOfBoids; i++) {
+  createBoid();
+}
+
+updateBoids();
