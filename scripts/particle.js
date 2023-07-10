18c39b20dad0a7c9fe02c8aeceb52eda4629b62d
zVima <115565123+zVima@users.noreply.github.com>
2023-07-10 15:46:42 +0200

    Update particle.js

--- scripts/particle.js
+++ scripts/particle.js
@@ -1,16 +1,16 @@
 class Particle {
-  constructor(position, velocity, acceleration) {
+  constructor(position, velocity, acceleration, linePositions, index, positionsMaxLength) {
     this.position = position;
     this.velocity = velocity;
     this.acceleration = acceleration;
-    this.maxSpeed = 5;
-    this.color = { r: 0, g: 0, b: 0 }
+    this.maxSpeed = 1;
 
-    this.maxLength = 100;
+    this.maxLength = positionsMaxLength;
     this.positions = [];
-    this.positions.push(this.position.clone());
 
-    /* this.prevPos = this.position.clone(); */
+    this.linePositions = linePositions;
+    this.index = index;
+    /* this.initLinePositions(); */
   }
 
   update(width, height, deltaTime) {
@@ -19,25 +19,12 @@ class Particle {
     this.position.add(this.velocity);
     this.acceleration.multiplyScalar(0);
 
-    this.color = this.calculateColor(width, height);
-
     this.wrap(width, height);
 
     this.positions.push(this.position.clone());
     if (this.positions.length > this.maxLength) {
       this.positions.shift();
     }
-  }
-
-  calculateColor(width, height) {
-    const normalizedX = this.position.x / width;
-    const normalizedY = this.position.y / height;
-
-    const r = Math.floor(normalizedX * 30) + 25; // Darker red towards the right
-    const g = Math.floor((1 - normalizedY) * 150) + 100; // Brighter green towards the top
-    const b = 255;
-
-    return { r, g, b };
   }
 
   follow(vectors, scl, cols) {
@@ -54,54 +41,61 @@ class Particle {
     this.acceleration.add(force);
   }
 
-  show(context) {
-    context.strokeStyle = `rgb(${this.color.r}, ${this.color.g}, ${this.color.b})`;
-    context.globalAlpha = 0.5;
-    context.lineWidth = 25;
-
-    context.beginPath();
-
-    for (let i = 1; i < this.positions.length; i++) {
-      const prevPosition = this.positions[i - 1];
-      const currentPosition = this.positions[i];
-      const distance = prevPosition.distanceTo(currentPosition);
-
-      if (distance < context.lineWidth * 2) {
-        context.lineTo(currentPosition.x, currentPosition.y);
-      } else {
-        context.beginPath();
-        context.moveTo(currentPosition.x, currentPosition.y);
-      }
-    }
-
-    context.stroke();
-
-    /* context.strokeStyle = `rgb(${this.color.r}, ${this.color.g}, ${this.color.b})`;
-    context.globalAlpha = 0.1;
-    context.lineWidth = 2;
-    context.beginPath();
-    context.moveTo(this.prevPos.x, this.prevPos.y);
-    context.lineTo(this.position.x, this.position.y);
-    context.stroke(); */
-    /* context.fillRect(this.position.x, this.position.y, 1, 1); */
-
-    /* this.prevPos = this.position.clone(); */
-  }
-
   wrap(width, height) {
     if (this.position.x > width) {
       this.position.x = 0;
-      this.prevPos = this.position.clone();
     } else if (this.position.x < 0) {
       this.position.x = width;
-      this.prevPos = this.position.clone();
     }
     if (this.position.y > height) {
       this.position.y = 0;
-      this.prevPos = this.position.clone();
     } else if (this.position.y < 0) {
       this.position.y = height;
-      this.prevPos = this.position.clone();
+    }
+  }
+
+  initLinePositions() {
+    const startIndex = this.index * (this.maxLength * 3);
+
+    for (let i = 0; i < this.maxLength; i++) {
+      const posIndex = startIndex + i * 3;
+
+      this.linePositions[posIndex + 0] = this.position.x;
+      this.linePositions[posIndex + 1] = this.position.y;
+      this.linePositions[posIndex + 2] = 0;
+    }
+  }
+
+  updateLinePositions() {
+    const startIndex = this.index * (this.maxLength * 3);
+
+    if (this.positions.length !== this.maxLength) {
+      for (let i = this.position.length - 1; i < this.maxLength; i++) {
+        const posIndex = startIndex + i * 3;
+
+        this.linePositions[posIndex + 0] = this.position.x;
+        this.linePositions[posIndex + 1] = this.position.y;
+        this.linePositions[posIndex + 2] = 0;
+      }
+    }
+
+    for (let i = 0; i < this.positions.length; i++) {
+
+      const currentPos = this.positions[i];
+      const posIndex = startIndex + i * 3;
+
+      this.linePositions[posIndex + 0] = currentPos.x;
+      this.linePositions[posIndex + 1] = currentPos.y;
+      this.linePositions[posIndex + 2] = 0;
+
+      /* if (this.linePositions[posIndex + 3] === 0 &&
+        this.linePositions[posIndex + 4] === 0) {
+        for (let j = posIndex; j < this.maxLength - i - 1; j++) {
+          const updateIndex = posIndex + j * 3;
+          this.linePositions[updateIndex + 0] = currentPos.x;
+          this.linePositions[updateIndex + 1] = currentPos.y;
+        }
+      } */
     }
   }
 }
