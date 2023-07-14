de14bb1d5081f87ae2cfcd17b4df805fefa8adf4
zVima <115565123+zVima@users.noreply.github.com>
2023-07-14 15:34:48 +0200

    Add files via upload

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,50 +1,34 @@
-/* const sections = document.querySelectorAll("section");
+// Register the ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
 
-// this scrolling object just allows us to conveniently call scrolling.enable(), scrolling.disable(), and check if scrolling.enabled is true. 
-// some browsers (like iOS Safari) handle scrolling on a separate thread and can cause things to get out of sync (jitter/jumpy), so when we're animating the scroll position, force an update of GSAP tweens when there's a scroll event in order to maintain synchronization)
-const scrolling = {
-    enabled: true,
-    events: "scroll,wheel,touchmove,pointermove".split(","),
-    prevent: e => e.preventDefault(),
-    disable() {
-      if (scrolling.enabled) {
-        scrolling.enabled = false;
-        window.addEventListener("scroll", gsap.ticker.tick, {passive: true});
-        scrolling.events.forEach((e, i) => (i ? document : window).addEventListener(e, scrolling.prevent, {passive: false}));
-      }
-    },
-    enable() {
-      if (!scrolling.enabled) {
-        scrolling.enabled = true;
-        window.removeEventListener("scroll", gsap.ticker.tick);
-        scrolling.events.forEach((e, i) => (i ? document : window).removeEventListener(e, scrolling.prevent));
-      }
-    }
-};
+// Function to animate the title
+function animateTitle() {
+  const title = document.querySelectorAll('.title');
 
-function goToSection(section, anim, i) {
-  if (scrolling.enabled) { // skip if a scroll tween is in progress
-    scrolling.disable();
-    console.log(window);
-    gsap.to(window, {
-      scrollTo: {y: section, autoKill: false},
-      onComplete: scrolling.enable,
-      duration: 1
-    });
+  title.forEach((current) => {
+    gsap.fromTo(
+      current,
+      { y: '100px' },
+      { y: 0, duration: 1 }
+    );
 
-    anim && anim.restart();
-  }
+    gsap.fromTo(
+      current,
+      { opacity: 0 },
+      { opacity: 1, duration: 5 }
+    );
+  });
 }
 
-sections.forEach((section, i) => {
-  const intoAnim = gsap.from(section.querySelector(".right-col"), {yPercent: 50, duration: 1, paused: true});
-  
-  ScrollTrigger.create({
-    trigger: section,
-    start: "top bottom-=1",
-    end: "bottom top+=1",
-    onEnter: () => goToSection(section, intoAnim),
-    onEnterBack: () => goToSection(section)
+// Function to bind the animation to ScrollTrigger
+function bindAnimationToScrollTrigger() {
+  ScrollTrigger.batch(".title", {
+    onEnter: animateTitle, // Trigger the animation when the element enters the viewport
+    start: "bottom 80%", // Adjust the start position as needed
+    end: "bottom 20%", // Adjust the end position as needed
+    once: true // Play the animation only once
   });
-});
- */
+}
+
+// Call the function to bind the animation to ScrollTrigger
+bindAnimationToScrollTrigger();
